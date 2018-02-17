92d6759ecca1ca14d1ded12b3c84c30a1db5e43d
Justyn <egot@istic.pw>
2018-02-17 16:19:50 -0600

    Add sha256.lua

--- uk/main.lua
+++ uk/main.lua
@@ -6,12 +6,15 @@ local ghrequire = require("uk.dep.ghrequire")
 local jua = ghrequire("justync7","Jua","jua.lua")
 local await = jua.await
 
+local sha256 = require("uk.crypto.sha256")
+
 jua.on("terminate", function()
   print("Terminated")
   jua.stop()
 end)
 
 jua.go(function()
+  print(sha256("A"))
   log("Info", LogType.INFO)
   log("Debug", LogType.DEBUG)
   log("Error", LogType.ERROR)
